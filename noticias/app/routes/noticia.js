ea577d0dc4e54c24f8ff871bd157ed573e43109c
rafa <magalhas.rafa@gmail.com>
2017-08-31 00:45:57 -0300

    Add mysql connection

--- noticias/app/routes/noticia.js
+++ noticias/app/routes/noticia.js
@@ -1,5 +1,11 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function(app){
+	var connection = dbConnection();
+
 	app.get('/noticias', function(req, res){
-		res.render("noticias/noticias");
+		connection.query('select * from noticias', function(err, result){
+			res.render("noticias/noticias", {noticias : result});
+		});
 	});
 }
