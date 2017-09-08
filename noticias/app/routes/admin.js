b02b7e13da1fbfacfb44813286425fb5fd4e209b
rafa <magalhas.rafa@gmail.com>
2017-09-08 00:27:20 -0300

    Add express validator to validates the page inputs

--- noticias/app/routes/admin.js
+++ noticias/app/routes/admin.js
@@ -5,6 +5,20 @@ module.exports = function(application){
 
 	application.post('/noticias/salvar', function(req, res){
 		var noticia = req.body;
+		
+		req.assert('titulo', 'Título é obrigatório').notEmpty();
+		req.assert('resumo', 'Resumo é obrigatório e deve conter de 10 à 100 caracteres').notEmpty().len(10, 100); 
+		req.assert('autor', 'Autor é obrigatória').notEmpty();
+		req.assert('data_noticia', 'Data é obrigatória').notEmpty();//.isDate({format: 'YYYY-MM-DD'});
+		req.assert('noticia', 'Conteúdo da notícia é obrigatória').notEmpty();
+
+		var error = req.validationErrors();
+		console.log(error);
+		if (error){
+			res.render("admin/form_add_noticia");
+			return;
+		}
+
 		var connection = application.config.dbConnection();
 		var noticiasModel = new application.app.models.NoticiasDAO(connection);
 
