2396ec87c87dd61132fe6246da22161d49ced663
Rafael Magalhães <magalhas.rafa@gmail.com>
2017-10-28 17:43:25 -0200

    Add api project and first api verb

--- instagram_clone/api/server.js
+++ instagram_clone/api/server.js
@@ -8,13 +8,34 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+var db = new mongodb.Db(
+  'instagram',
+  new mongodb.Server('localhost', 27017, {}),
+  {}
+);
+
 var port = 8080;
-
 app.listen(port);
-
+console.log('O servidor está escutnado na porta ' + port);
 
 app.get('/', function(req, res){
   res.send({msg: 'Olá'});
 });
 
-console.log('O servidor está escutnado na porta ' + port);
+app.post('/api', function(req, res){
+  var data = req.body;
+
+  db.open( function(err, mongoclient){
+    mongoclient.collection('posts', function(err, collection){
+      collection.insert(data, function(err, result){
+        if(err){
+          res.json(err);
+        } else{
+          res.json(result);
+        }
+        mongoclient.close;
+      });
+    });
+  });
+});
+
